5f91848d918e09a69433c90d3d2b05c79b63d487
Sabekur <sabekur2017@gmail.com>
2021-10-14 00:21:02 +0600

    compose first

---
+++ app/src/main/java/com/sabekur2017/composeuipractice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.sabekur2017.composeuipractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sabekur2017.composeuipractice.ui.theme.ComposeUiPracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeUiPracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Compose UI")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!",modifier = Modifier.padding(16.dp),color = Color.Green)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeUiPracticeTheme {
+        Greeting("Android")
+    }
+}
