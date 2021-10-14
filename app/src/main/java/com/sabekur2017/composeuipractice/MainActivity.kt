629a8be9c2efc6ad0715c9c739524a15ddf6800c
Sabekur <sabekur2017@gmail.com>
2021-10-15 01:00:42 +0600

    simple scrollable list

--- app/src/main/java/com/sabekur2017/composeuipractice/MainActivity.kt
+++ app/src/main/java/com/sabekur2017/composeuipractice/MainActivity.kt
@@ -3,40 +3,64 @@ package com.sabekur2017.composeuipractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.sabekur2017.composeuipractice.ui.theme.ComposeUiPracticeTheme
+import com.sabekur2017.composeuipractice.internals.columColor
+import  com.sabekur2017.composeuipractice.internals.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeUiPracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Compose UI")
-                }
-            }
+             MainContent()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!",modifier = Modifier.padding(16.dp),color = Color.Green)
-}
+fun MainContent(){
+    val scrollState= rememberScrollState()
+    LaunchedEffect(Unit){scrollState.animateScrollTo(10000)}
+    Column(modifier = Modifier
+        .background(columColor)
+        .fillMaxSize()
+        .verticalScroll(scrollState)) {
+        repeat(100){  counter->
+            Text(
+                text = "Counter : $counter",
+                fontSize = textFontSize,
+                color = textColor,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(normalRadius))
+                    .background(backgroundColor)
+                    .padding(normalElevation)
+                    .background(
+                        if (counter%2 == 0)evenColor
+                        else oddColor
+                    )
+                    .padding(size25dp)
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeUiPracticeTheme {
-        Greeting("Android")
     }
+}
+@Preview
+@Composable
+fun ComposablePreview(){
+    MainContent()
 }
