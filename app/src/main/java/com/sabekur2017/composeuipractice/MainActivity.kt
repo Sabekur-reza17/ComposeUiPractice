e02a5621c10c41abf44f9c73fc71521786312869
Reza <sabekur2017@gmail.com>
2023-01-06 00:46:00 +0600

    drop down example

--- app/src/main/java/com/sabekur2017/composeuipractice/MainActivity.kt
+++ app/src/main/java/com/sabekur2017/composeuipractice/MainActivity.kt
@@ -21,13 +21,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.sabekur2017.composeuipractice.internals.columColor
 import  com.sabekur2017.composeuipractice.internals.*
 import com.sabekur2017.composeuipractice.navigation.Navigation
+import com.sabekur2017.composeuipractice.uicomponent.DropDownDemo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
            //  MainContent()
-            Navigation()
+            DropDownDemo()
         }
     }
 }
